993936a8de3906de284ba965663bae7d271c4b79
Gabo Salmeron <gabosalmeron@192.168.1.64>
2026-06-04 18:30:35 -0600

    revert unneeded changes in model

--- pool/models.py
+++ pool/models.py
@@ -42,12 +42,11 @@ class User(AbstractUser):
     username = models.CharField(max_length=254, unique=True)
     email = models.EmailField(unique=True)
     authorized = models.BooleanField(
-        default=True,
+        default=False,
         help_text="Pagó y envió a tiempo; se marca a mano.",
     )
 
     USERNAME_FIELD = "email"
-    PASSWORD_FIELD = "password"
     REQUIRED_FIELDS = []
 
     objects = UserManager()
